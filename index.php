7792992cb66d6ff8f016220491b9473026b06548
Cristian1231233 <campagnacristian22@gmail.com>
2023-01-19 15:47:08 +0100

    condition commit

--- index.php
+++ index.php
@@ -10,9 +10,22 @@ $filterNode = [];
 $page_num = 0;
 $page_size = 0;
 
+
 if(isset($_GET['language'])){
     foreach($arrayNodes as $item){
         if($item['language'] === $_GET['language']){
+            $filterNode[] = $item;
+        }
+    }
+}elseif(isset($_GET['node_id'])){
+    foreach($arrayNodes as $item){
+        if($item['id'] === $_GET['node_id']){
+            $filterNode[] = $item;
+        }
+    }
+}elseif(isset($_GET['search_keyword'])){
+    foreach($arrayNodes as $item){
+        if($item['level'] > $_GET['search_keyword']){
             $filterNode[] = $item;
         }
     }
@@ -36,13 +49,14 @@ if(isset($_GET['language'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Backend Developer Task</title>
 </head>
 <body>
     <div class="myContainer">
         <?php foreach ($filterNode as $item) : ?>
             <div class="card">
-                <div>Id_node: <?php echo $item['id'] ?> </div>
+                <div>Node_id: <?php echo $item['id'] ?> </div>
+                <div>Level: <?php echo $item['level'] ?> </div>
                 <div>Language: <?php echo $item['language'] ?> </div>
                 <div>NodeName: <?php echo $item['nodeName'] ?> </div>
             </div>
